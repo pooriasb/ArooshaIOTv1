cdfda4000e64645d06d44084a347287e69ba0092
pooriasb <mr.sabooki@gmail.com>
2023-05-10 10:51:12 +0330

    add static to api gateway

--- ApiGateway/index.js
+++ ApiGateway/index.js
@@ -2,4 +2,5 @@ const express = require('express');
 const config = require('config');
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded);
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('Public'))
