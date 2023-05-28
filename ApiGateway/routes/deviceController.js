d994dd1dfd7f8f24a97996fc440bc10a1fadd1a8
pooriasb <mr.sabooki@gmail.com>
2023-05-28 12:55:59 +0330

    create get my device list api

--- ApiGateway/routes/deviceController.js
+++ ApiGateway/routes/deviceController.js
@@ -1,14 +1,46 @@
 const express = require('express');
 const router = express.Router();
-
+const config = require('config');
+const http = require('http');
 
 router.get('/sendMessage',(req,res)=>{
     res.sendStatus(200);
 });
 /***************************************************Device Management  */
-router.get('/getMyDeviceList', (req, res) => {
-    res.sendStatus(200);
+// it needs to select specific filds to return in the device micro service
+router.get('/getMyDeviceList/:userId', (req, res) => {
+    //http://127.0.0.1:3003/api/ctrl/list/sajad
+    getMyDeviceListFromService(req.params.userId).then((value)=>{
+        res.send(value);
+    });
+    
 });
+
+function getMyDeviceListFromService(userId) {
+    return p = new Promise((resolve, reject) => {
+
+        http.get(config.DeviceServiceAddress + '/api/ctrl/list/' + userId, (resp) => {
+            let data = "";
+            // A chunk of data has been recieved.
+            resp.on("data", chunk => {
+                data += chunk;
+            });
+            // The whole response has been received. Print out the result.
+            resp.on("end", () => {
+                //    let url = JSON.parse(data).message;
+
+                resolve(data);
+            });
+        }).on("error", err => {
+            console.log("Error: " + err.message);
+        });
+    });
+}
+
+
+
+
+
 router.get('/CreateDevice', (req, res) => {
     res.sendStatus(200);
 });
