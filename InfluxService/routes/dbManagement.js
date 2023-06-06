ac492f1c0de418f6995aec45d4955821d76a029f
pooriasb <mr.sabooki@gmail.com>
2023-06-06 11:11:08 +0330

    init influx service and add dbmanagement

---
+++ InfluxService/routes/dbManagement.js
@@ -0,0 +1,39 @@
+const { InfluxDB } = require('@influxdata/influxdb-client')
+
+// You can generate a Token from the "Tokens Tab" in the UI
+const token = 'XKlWnw2A09WNqISlYUuprBH9WryaFg6L31dtpjJ0EtZ6wFXUy1tLvb3y5ET9Wy2cHxZDoMF6lubvAlK3HPqUZA=='
+const org = 'Aroosha'
+const bucket = 'Aroosha'
+
+const client = new InfluxDB({ url: 'http://154.211.2.176:8086', token: token })
+
+
+const { Point } = require('@influxdata/influxdb-client')
+const writeApi = client.getWriteApi(org, bucket)
+writeApi.useDefaultTags({ host: 'host1' })
+
+
+function saveAliveSignal(userId,MAC,HUE,RGBBrightnes,ColorTemperature,Brightness,Dance) {
+    const point = new Point(userId)
+        .stringField('MAC', MAC)
+        .stringField('HUE', HUE)
+        .intField('RGBBrightnes ', RGBBrightnes)
+        .intField('ColorTemperature  ',ColorTemperature)
+        .intField('Brightness   ', Brightness)
+        .intField('Dance   ', Dance)
+
+
+    writeApi.writePoint(point)
+    writeApi
+        .close()
+        .then(() => {
+            console.log('Alive signal inserted')
+        })
+        .catch(e => {
+            console.error(e)
+            console.log('\\nFinished ERROR')
+        })
+}
+
+
+module.exports.saveAliveSignal = saveAliveSignal;
