76e34dba6f79a8c5fa4c8e7f11192d6e82d1759e
pooriasb <mr.sabooki@gmail.com>
2023-06-13 12:24:38 +0330

    health-config apigateway router

--- HealthService/index.js
+++ HealthService/index.js
@@ -1,1 +1,12 @@
 const express= require('express');
+const app = express();
+const apiRouter = require('./routes/apiGateway');
+
+app.use('/apiGateway',apiRouter);
+
+
+// run server
+const { PORT = 3000 } = process.env;
+app.listen(PORT, () => {
+  console.log(`Api Gateway is listening on port ${PORT}`);
+});
