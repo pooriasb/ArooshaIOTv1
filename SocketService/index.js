4f8d72c14a97d976306428b6fc61f6844b151cf3
pooriasb <mr.sabooki@gmail.com>
2023-06-19 10:42:29 +0330

    fix send message bug

--- SocketService/index.js
+++ SocketService/index.js
@@ -92,14 +92,10 @@ io.on('connection', (socket) => {
 
 app.post('/sendMessage', async (req, res) => {
   try {
-
-    console.log('body : ' + req.body);
-
-
-    //     io.to(req.body.mac).emit('response', req.body.message);
-    //     console.log('send Message mac :' + req.body.mac);
-    //    var response =  await axios.post(configfile.LogAddress + '/api/log/logMessage',{mac:req.body.mac, message:req.body.message });
-    //  console.log(`message : ${req.body.message} log service respons: ${response.data}`);
+    io.to(req.body.mac).emit('response', req.body.message);
+    console.log('send Message mac :' + req.body.mac);
+    var response = await axios.post(configfile.LogAddress + '/api/log/logMessage', { mac: req.body.mac, message: req.body.message });
+    console.log(`message : ${req.body.message} log service respons: ${response.data}`);
     res.sendStatus(200);
   } catch (err) {
     console.error(err);
