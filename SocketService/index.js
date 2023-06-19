9ac5ddd49180b5dfd9c21471e6f7974daf476210
pooriasb <mr.sabooki@gmail.com>
2023-06-19 10:50:20 +0330

    comment more

--- SocketService/index.js
+++ SocketService/index.js
@@ -92,10 +92,11 @@ io.on('connection', (socket) => {
 
 app.post('/sendMessage', async (req, res) => {
   try {
+    console.log(req.body);
     //io.to(req.body.mac).emit('response', req.body.message);
-    console.log('send Message mac :' + req.body.mac);
+   // console.log('send Message mac :' + req.body.mac);
    // var response = await axios.post(configfile.LogAddress + '/api/log/logMessage', { mac: req.body.mac, message: req.body.message });
-    console.log(`message : ${req.body.message} log service respons: ${response.data}`);
+    //console.log(`message : ${req.body.message} log service respons: ${response.data}`);
     res.sendStatus(200);
   } catch (err) {
     console.error(err);
