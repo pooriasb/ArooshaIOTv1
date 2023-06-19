336f242d7cf45ae432fad6381f28fac5e6edf595
pooriasb <mr.sabooki@gmail.com>
2023-06-19 15:20:07 +0330

    delete schedule

--- ApiGateway/routes/schedule.js
+++ ApiGateway/routes/schedule.js
@@ -24,14 +24,15 @@ router.post('/setScheduleStatus', async (req, res) => {
     });
     res.send(response.data);
 });
-router.post('/updateShedule',async (req, res) => {
+router.post('/updateShedule', async (req, res) => {
     const { isOnce, weekDays, hour, minute, events, scheduleId } = req.body;
     var response = await axios.post(config.SchedulerAddress + '/api/scheduler/CreateScheduler', {
-        isOnce, weekDays, hour, minute, events ,scheduleId
+        isOnce, weekDays, hour, minute, events, scheduleId
     });
     res.send(response.data);
 });
-router.post('/deleteSchedule/:scheduleId', (req, res) => {
-    res.sendStatus(200);
+router.post('/deleteSchedule/:scheduleId', async (req, res) => {
+    var response = await axios.get(config.SchedulerAddress + '/api/scheduler/deleteSchedule/' + req.params.scheduleId);
+    res.send(response.data);
 });
 module.exports = router;
