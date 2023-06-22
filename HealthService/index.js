45321ebcde6ebc52df283c591c89bb09bcb198f3
pooriasb <mr.sabooki@gmail.com>
2023-06-22 14:51:43 +0330

    log files

--- HealthService/index.js
+++ HealthService/index.js
@@ -10,6 +10,7 @@ const socketRouter = require('./routes/socket');
 const pinger = require('./routes/pinger');
 const config = require('config');
 const cron = require('node-cron');
+const fs = require('fs');
 
 app.use('/apiGateway', apiRouter);
 app.use('/auth', authRouter);
@@ -22,33 +23,333 @@ app.use('/mqtt', mqttRouter);
 
 
 cron.schedule('*/10 * * * * *', async () => {
-console.clear();
-console.log(Date.now());
+  console.clear();
+  console.log(Date.now());
 
   pinger.pingRequest(config.ApiGatewayAddress + '/pinger').then((value) => {
-    console.log('ApiGateway :' +value);
+
+    const now = new Date(Date.now());
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+    const fileName = `./logs/APIlog.txt`;
+
+    let lastStatus;
+    try {
+      const lastLogEntry = fs.readFileSync(fileName, 'utf8')
+        .split('\n')
+        .filter(Boolean)
+        .pop();
+      lastStatus = lastLogEntry.split('Status:')[1].split(' ')[0];
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.log(`File "${fileName}" does not exist, creating it...`);
+        fs.writeFileSync(fileName, '');
+        lastStatus = null;
+      } else {
+        console.error('Error while reading the log file:', error);
+        lastStatus = null;
+      }
+    }
+
+
+
+    if (lastStatus === undefined || value.status.toString() !== lastStatus) {
+      const logEntry = `${formattedDate} - Status:${value.status} Data:${value.data}\n`;
+      fs.appendFileSync(fileName, logEntry);
+      console.log(`API Log created: ${logEntry}`);
+    } else {
+      console.log('API No change in status, not logging.')
+    }
   });
   pinger.pingRequest(config.DeviceServiceAddress + '/pinger').then((value) => {
-    console.log('DeviceService :' +value);
+    const now = new Date(Date.now());
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+  
+    const formattedDate = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+    const fileName = `./logs/Devicelog.txt`;
+  
+    let lastStatus;
+    try {
+      const lastLogEntry = fs.readFileSync(fileName, 'utf8')
+        .split('\n')
+        .filter(Boolean)
+        .pop();
+      lastStatus = lastLogEntry.split('Status:')[1].split(' ')[0];
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.log(`File "${fileName}" does not exist, creating it...`);
+        fs.writeFileSync(fileName, '');
+        lastStatus = null;
+      } else {
+        console.error('Error while reading the log file:', error);
+        lastStatus = null;
+      }
+    }
+  
+  
+  
+    if (lastStatus === undefined || value.status.toString() !== lastStatus) {
+      const logEntry = `${formattedDate} - Status:${value.status} Data:${value.data}\n`;
+      fs.appendFileSync(fileName, logEntry);
+      console.log(`Device Log created: ${logEntry}`);
+    } else {
+      console.log('Device No change in status, not logging.')
+    }
   });
   pinger.pingRequest(config.MQTTServiceAddress + '/pinger').then((value) => {
-    console.log('MQTTService :' +value);
+    const now = new Date(Date.now());
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+    const fileName = `./logs/MQTTlog.txt`;
+
+    let lastStatus;
+    try {
+      const lastLogEntry = fs.readFileSync(fileName, 'utf8')
+        .split('\n')
+        .filter(Boolean)
+        .pop();
+      lastStatus = lastLogEntry.split('Status:')[1].split(' ')[0];
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.log(`File "${fileName}" does not exist, creating it...`);
+        fs.writeFileSync(fileName, '');
+        lastStatus = null;
+      } else {
+        console.error('Error while reading the log file:', error);
+        lastStatus = null;
+      }
+    }
+
+
+
+    if (lastStatus === undefined || value.status.toString() !== lastStatus) {
+      const logEntry = `${formattedDate} - Status:${value.status} Data:${value.data}\n`;
+      fs.appendFileSync(fileName, logEntry);
+      console.log(`MQTT Log created: ${logEntry}`);
+    } else {
+      const logEntry = `${formattedDate} - Status:${value.status} Data:${value.data}\n`;
+
+      console.log('MQTT not logging.' +logEntry );
+    }
+
   });
-  
   pinger.pingRequest(config.SocketAddress + '/pinger').then((value) => {
-    console.log('Socket :' +value);
+    const now = new Date(Date.now());
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+    const fileName = `./logs/SOCKETlog.txt`;
+
+    let lastStatus;
+    try {
+      const lastLogEntry = fs.readFileSync(fileName, 'utf8')
+        .split('\n')
+        .filter(Boolean)
+        .pop();
+      lastStatus = lastLogEntry.split('Status:')[1].split(' ')[0];
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.log(`File "${fileName}" does not exist, creating it...`);
+        fs.writeFileSync(fileName, '');
+        lastStatus = null;
+      } else {
+        console.error('Error while reading the log file:', error);
+        lastStatus = null;
+      }
+    }
+
+
+
+    if (lastStatus === undefined || value.status.toString() !== lastStatus) {
+      const logEntry = `${formattedDate} - Status:${value.status} Data:${value.data}\n`;
+      fs.appendFileSync(fileName, logEntry);
+      console.log(`socket Log created: ${logEntry}`);
+    } else {
+      console.log('socket No change in status, not logging.')
+    }
+
   });
   pinger.pingRequest(config.LogAddress + '/pinger').then((value) => {
-    console.log('log :' +value);
+    const now = new Date(Date.now());
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+    const fileName = `./logs/LogServicelog.txt`;
+
+    let lastStatus;
+    try {
+      const lastLogEntry = fs.readFileSync(fileName, 'utf8')
+        .split('\n')
+        .filter(Boolean)
+        .pop();
+      lastStatus = lastLogEntry.split('Status:')[1].split(' ')[0];
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.log(`File "${fileName}" does not exist, creating it...`);
+        fs.writeFileSync(fileName, '');
+        lastStatus = null;
+      } else {
+        console.error('Error while reading the log file:', error);
+        lastStatus = null;
+      }
+    }
+
+
+
+    if (lastStatus === undefined || value.status.toString() !== lastStatus) {
+      const logEntry = `${formattedDate} - Status:${value.status} Data:${value.data}\n`;
+      fs.appendFileSync(fileName, logEntry);
+      console.log(`LOG Log created: ${logEntry}`);
+    } else {
+      console.log('LOG No change in status, not logging.')
+    }
   });
   pinger.pingRequest(config.EnergyAddress + '/pinger').then((value) => {
-    console.log('Energy :' +value);
+    const now = new Date(Date.now());
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+    const fileName = `./logs/Energylog.txt`;
+
+    let lastStatus;
+    try {
+      const lastLogEntry = fs.readFileSync(fileName, 'utf8')
+        .split('\n')
+        .filter(Boolean)
+        .pop();
+      lastStatus = lastLogEntry.split('Status:')[1].split(' ')[0];
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.log(`File "${fileName}" does not exist, creating it...`);
+        fs.writeFileSync(fileName, '');
+        lastStatus = null;
+      } else {
+        console.error('Error while reading the log file:', error);
+        lastStatus = null;
+      }
+    }
+
+
+
+    if (lastStatus === undefined || value.status.toString() !== lastStatus) {
+      const logEntry = `${formattedDate} - Status:${value.status} Data:${value.data}\n`;
+      fs.appendFileSync(fileName, logEntry);
+      console.log(`Energy Log created: ${logEntry}`);
+    } else {
+      console.log('Energy No change in status, not logging.')
+    }
   });
   pinger.pingRequest(config.schedulerAddress + '/pinger').then((value) => {
-    console.log('scheduler :' +value);
+    const now = new Date(Date.now());
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+    const fileName = `./logs/SCHEDULERlog.txt`;
+
+    let lastStatus;
+    try {
+      const lastLogEntry = fs.readFileSync(fileName, 'utf8')
+        .split('\n')
+        .filter(Boolean)
+        .pop();
+      lastStatus = lastLogEntry.split('Status:')[1].split(' ')[0];
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.log(`File "${fileName}" does not exist, creating it...`);
+        fs.writeFileSync(fileName, '');
+        lastStatus = null;
+      } else {
+        console.error('Error while reading the log file:', error);
+        lastStatus = null;
+      }
+    }
+
+
+
+    if (lastStatus === undefined || value.status.toString() !== lastStatus) {
+      const logEntry = `${formattedDate} - Status:${value.status} Data:${value.data}\n`;
+      fs.appendFileSync(fileName, logEntry);
+      console.log(`Scheduler Log created: ${logEntry}`);
+    } else {
+      console.log('Scheduler No change in status, not logging.')
+    }
   });
   pinger.pingRequest(config.AuthAddress + '/pinger').then((value) => {
-    console.log('Auth :' + value);
+    const now = new Date(Date.now());
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+    const fileName = `./logs/AUTHlog.txt`;
+
+    let lastStatus;
+    try {
+      const lastLogEntry = fs.readFileSync(fileName, 'utf8')
+        .split('\n')
+        .filter(Boolean)
+        .pop();
+      lastStatus = lastLogEntry.split('Status:')[1].split(' ')[0];
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.log(`File "${fileName}" does not exist, creating it...`);
+        fs.writeFileSync(fileName, '');
+        lastStatus = null;
+      } else {
+        console.error('Error while reading the log file:', error);
+        lastStatus = null;
+      }
+    }
+
+
+
+    if (lastStatus === undefined || value.status.toString() !== lastStatus) {
+      const logEntry = `${formattedDate} - Status:${value.status} Data:${value.data}\n`;
+      fs.appendFileSync(fileName, logEntry);
+      console.log(`AUTH Log created: ${logEntry}`);
+    } else {
+      console.log('AUTH No change in status, not logging.')
+    }
   });
 
 }, { scheduled: true });
--- HealthService/routes/pinger.js
+++ HealthService/routes/pinger.js
@@ -5,17 +5,25 @@ const axios = require('axios');
 const pingRequest = async (to) => {
   try {
     const response = await axios.get(to);
-    if(response.data)
-    return response.data;
-    else return "no data recived";
+
+    var result = {
+      status: response.status,
+      data: response.data
+    }
+    return result;
+
   } catch (error) {
-  
-    return error.message; // Change the return value to suit your needs
+
+    var result = {
+      status: 500,
+      data: error.message
+    }
+    return result;
   }
 };
 
 
 
 module.exports = {
-    pingRequest
+  pingRequest
 }
