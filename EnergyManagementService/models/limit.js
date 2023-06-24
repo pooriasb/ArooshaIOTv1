f5c06565f2ac723beae4df2186de9bbc5b26d7a7
pooriasb <mr.sabooki@gmail.com>
2023-06-24 12:15:12 +0330

    create limit

--- EnergyManagementService/models/limit.js
+++ EnergyManagementService/models/limit.js
@@ -2,16 +2,16 @@ const express = require('express');
 const _ = require('lodash');
 const mongoose = require('mongoose');
 const config = require('config');
-
+const report = require('../routes/reports');
 
 
 mongoose.connect(config.dbAddress)
     .then(() => console.log('Connected to database'))
     .catch(err => console.log('Error ' + err));
 const limitSchema = new mongoose.Schema({
-    userId:String,
+    userId: String,
     createDate: Date,
-    isActive :Boolean,
+    isActive: Boolean,
     deviceMac: String,
     deviceName: String,
     maxUsePower: String,
@@ -22,76 +22,96 @@ const Limits = mongoose.model('Limits', limitSchema);
 
 
 // Create a new limit entry
-async function createLimit({ userId,deviceMac, deviceName, maxUsePower, dimmer }) {
-  try {
-    const limit = new Limits({userId,isActive:true, createDate: Date.now(), deviceMac, deviceName, maxUsePower, dimmer });
-    const result = await limit.save();
-    return result;
-  } catch (error) {
-    console.error(error.message);
-    return error;
-  }
+async function createLimit({ userId, deviceMac, deviceName, maxUsePower, dimmer }) {
+    try {
+        const limit = new Limits({ userId, isActive: true, createDate: Date.now(), deviceMac, deviceName, maxUsePower, dimmer });
+        const result = await limit.save();
+        return result;
+    } catch (error) {
+        console.error(error.message);
+        return error;
+    }
 }
 
 // Retrieve all limit entries
 async function getAllLimits(userId) {
-  try {
-    const limits = await Limits.find({userId:userId});
-    return limits;
-  } catch (error) {
-    console.error(error.message);
-    return error;
-  }
+    try {
+        const limits = await Limits.find({ userId: userId });
+        return limits;
+    } catch (error) {
+        console.error(error.message);
+        return error;
+    }
 }
 
 // Retrieve a specific limit entry by ID
 async function getLimitById(id) {
-  try {
-    const limit = await Limits.findById(id);
-    return limit;
-  } catch (error) {
-    console.error(error.message);
-    return error;
-  }
+    try {
+        const limit = await Limits.findById(id);
+        return limit;
+    } catch (error) {
+        console.error(error.message);
+        return error;
+    }
 }
 
 // Update a specific limit entry by ID
 async function updateLimitById(id, { createDate, deviceMac, deviceName, maxUsePower, dimmer }) {
-  try {
-    const limit = await Limits.findById(id);
-    limit.createDate = createDate;
-    limit.deviceMac = deviceMac;
-    limit.deviceName = deviceName;
-    limit.maxUsePower = maxUsePower;
-    limit.dimmer = dimmer;
-    const result = await limit.save();
-    return result;
-  } catch (error) {
-    console.error(error.message);
-    return error;
-  }
+    try {
+        const limit = await Limits.findById(id);
+        limit.createDate = createDate;
+        limit.deviceMac = deviceMac;
+        limit.deviceName = deviceName;
+        limit.maxUsePower = maxUsePower;
+        limit.dimmer = dimmer;
+        const result = await limit.save();
+        return result;
+    } catch (error) {
+        console.error(error.message);
+        return error;
+    }
 }
 
 // Delete a specific limit entry by ID
 async function deleteLimitById(id) {
-  try {
-    const result = await Limits.findByIdAndDelete(id);
-    return result;
-  } catch (error) {
-    console.error(error.message);
-    return error;
-  }
+    try {
+        const result = await Limits.findByIdAndDelete(id);
+        return result;
+    } catch (error) {
+        console.error(error.message);
+        return error;
+    }
 }
 async function setLimitActive(id, isActive) {
-  try {
-    const result = await Limits.findByIdAndUpdate(id, {isActive}, {new: true});
-    return result;
-  } catch (error) {
-    console.error(error.message);
-    return error;
-  }
+    try {
+        const result = await Limits.findByIdAndUpdate(id, { isActive }, { new: true });
+        return result;
+    } catch (error) {
+        console.error(error.message);
+        return error;
+    }
 }
 
+async function LimitCheck(id) {
+  var limit = await getLimitById(id);
+  var timeFromNow = Date.now() - new Date(limit.createDate);
+  var timeUnits = [
+    { unit: "d", ms: 86400000 }, // 24 hours in milliseconds
+    { unit: "h", ms: 3600000 }, // 60 minutes in milliseconds
+    { unit: "m", ms: 60000 }, // 60 seconds in milliseconds
+  ];
+  var timeString = "";
+  for (var i = 0; i < timeUnits.length; i++) {
+    var unit = timeUnits[i].unit;
+    var ms = timeUnits[i].ms;
+    var value = Math.floor(timeFromNow / ms);
+    if (value > 0) {
+      timeString += value + unit + " ";
+      timeFromNow -= value * ms;
+    }
+  }
+  report.energyUsageByDevice(limit.deviceMac, timeString);
+}
 
 
 module.exports = {
--- EnergyManagementService/routes/limitReport.js
+++ EnergyManagementService/routes/limitReport.js
@@ -3,11 +3,12 @@ const router = express.Router();
 router.use(express.json());
 const axios = require('axios');
 const config = require('config');
-
-const limitByDevice = () =>{
-    
-}
-
+const limit = require('../models/limit');
+router.post('/createLimit', async (req, res) => {
+    const { deviceMac, deviceName, maxUsePower, dimmer } = req.body;
+    var result = await limit.createLimit({ 'sajad', deviceMac, deviceName, maxUsePower, dimmer });
+    res.send(result);
+});
 
 
 module.exports = router;
