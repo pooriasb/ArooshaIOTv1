acad4caf1f0a0545acbeeccc9dd9a0ed5252c39c
pooriasb <mr.sabooki@gmail.com>
2023-07-01 16:24:16 +0330

    back to http

--- ApiGateway/index.js
+++ ApiGateway/index.js
@@ -1,22 +1,20 @@
-// const app = require('./app');
-// const cluster = require('cluster');
-// const os = require('os');
+const app = require('./app');
+const cluster = require('cluster');
+const os = require('os');
+if (cluster.isMaster) {
+  const cpuCount = os.cpus().length;
 
+  for (let i = 0; i < cpuCount; i++) {
+    cluster.fork();
+  }
+} else {
+  const { PORT = 3000 } = process.env;
+app.get('/pinger',(req,res)=>{res.send(`ApiGateWay is ok `)});
 
-// if (cluster.isMaster) {
-//   const cpuCount = os.cpus().length;
-
-//   for (let i = 0; i < cpuCount; i++) {
-//     cluster.fork();
-//   }
-// } else {
-//   const { PORT = 3000 } = process.env;
-// app.get('/pinger',(req,res)=>{res.send(`ApiGateWay is ok `)});
-
-//   app.listen(PORT, () => {
-//     console.log(`Api Gateway is listening on port: ${PORT}`);
-//   });
-// }
+  app.listen(PORT, () => {
+    console.log(`Api Gateway is listening on port: ${PORT}`);
+  });
+}
 
 
 
@@ -56,39 +54,39 @@
 
 
 
-const fs = require('fs');
-const https = require('https');
-const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
-const { exec } = require('child_process');
+// const fs = require('fs');
+// const https = require('https');
+// const express = require('express');
+// const cluster = require('cluster');
+// const os = require('os');
+// const { exec } = require('child_process');
 
-if (cluster.isMaster) {
-  const cpuCount = os.cpus().length;
+// if (cluster.isMaster) {
+//   const cpuCount = os.cpus().length;
 
-  for (let i = 0; i < cpuCount; i++) {
-    cluster.fork();
-  }
-} else {
-  const { PORT = 3000 } = process.env;
+//   for (let i = 0; i < cpuCount; i++) {
+//     cluster.fork();
+//   }
+// } else {
+//   const { PORT = 3000 } = process.env;
 
-  // Generate self-signed SSL certificates using OpenSSL
-  exec('openssl req -x509 -newkey rsa:2048 -nodes -keyout key.pem -out certificate.pem -days 365 -subj "/CN=example.com"');
+//   // Generate self-signed SSL certificates using OpenSSL
+//   exec('openssl req -x509 -newkey rsa:2048 -nodes -keyout key.pem -out certificate.pem -days 365 -subj "/CN=example.com"');
 
-  const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('certificate.pem')
-  };
+//   const options = {
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync('certificate.pem')
+//   };
 
-  const app = express();
+//   const app = express();
 
-  app.get('/pinger', (req, res) => {
-    res.send('ApiGateWay is ok');
-  });
+//   app.get('/pinger', (req, res) => {
+//     res.send('ApiGateWay is ok');
+//   });
 
-  const server = https.createServer(options, app);
+//   const server = https.createServer(options, app);
 
-  server.listen(PORT, () => {
-    console.log(`Api Gateway is listening on port: ${PORT}`);
-  });
-}
+//   server.listen(PORT, () => {
+//     console.log(`Api Gateway is listening on port: ${PORT}`);
+//   });
+// }
