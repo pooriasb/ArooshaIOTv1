3532aa66705bd3bc4dfc17208b248cda5e6378df
pooriasb <mr.sabooki@gmail.com>
2023-08-03 13:15:22 +0330

    fix a mistace

--- ApiGateway/routes/deviceController.js
+++ ApiGateway/routes/deviceController.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const config = require('config');
-const cache = require('memory-cache');
 const NodeCache = require('node-cache');
 
 //const { authMiddleware } = require('../middleware/auth');
